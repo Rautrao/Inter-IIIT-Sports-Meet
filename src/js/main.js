a57c6d7a8def8e1bc9783e322f39092baabf00c8
Rautrao <rautraoambadas@gmail.com>
2026-08-29 17:23:53 +0530

    Delete src directory
    deleted first try

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import { Navbar } from './components/navbar.js';
-import { ScrollReveal, animateCounters } from './components/animations.js';
-document.addEventListener('DOMContentLoaded', () => {
-  new Navbar();
-  new ScrollReveal();
-  animateCounters();
-});
